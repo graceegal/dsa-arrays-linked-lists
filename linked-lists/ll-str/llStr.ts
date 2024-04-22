b999c433b17e93dcb5d9e49b1cf49a7102f87366
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-22 12:10:05 -0700

    intial commit

---
+++ linked-lists/ll-str/llStr.ts
@@ -0,0 +1,158 @@
+/** IndexError: raised when index not found. */
+
+class IndexError extends Error {
+}
+
+/**
+ * NodeStr: node for a singly-linked list of string.
+ *
+ * - val
+ * - next: next NodeStr or null
+ */
+
+class NodeStr {
+  val: string;
+  next: NodeStr | null;
+
+  constructor(val: string) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/**
+ * Linked list of numbers.
+ */
+
+class LLStr {
+  head: NodeStr | null;
+  tail: NodeStr | null;
+  length: number;
+
+  constructor(vals: string[] = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (const val of vals) this.push(val);
+  }
+
+  /** push(val): add new value to end of list. */
+
+  push(val: string): void {
+    const newNode = new NodeStr(val);
+
+    if (this.head === null) this.head = newNode;
+
+    if (this.tail !== null) this.tail.next = newNode;
+
+    this.tail = newNode;
+    this.length++;
+  }
+
+  /** unshift(val): add new value to start of list. */
+
+  unshift(val: string): void {
+    const newNode = new NodeStr(val);
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    if (this.tail === null) this.tail = this.head;
+    this.length++;
+  }
+
+  /** pop(): return & remove last item.
+   *
+   * Throws IndexError on empty list.
+   **/
+
+  pop(): string {
+    if (this.head === null) throw new IndexError;
+
+    let curr: (NodeStr | null) = this.head;
+    let returnVal;
+
+    if (this.length === 1) {
+      returnVal = curr.val;
+      this.head = this.tail = null;
+      this.length--;
+      return returnVal;
+    }
+
+    while (curr !== null && curr.next !== this.tail) {
+      curr = curr.next;
+    }
+
+    returnVal = curr?.next?.val;
+    curr!.next = null;
+    this.tail = curr;
+
+    this.length--;
+    return returnVal!;
+  }
+
+  /** shift(): return & remove first item.
+   *
+   * Throws IndexError on empty list.
+   **/
+
+  shift(): string {
+    return "x";
+  }
+
+  /** getAt(idx): get val at idx.
+   *
+   * Throws IndexError if not found.
+   **/
+
+  getAt(idx: number): string {
+    return "x";
+  }
+
+  /** setAt(idx, val): set val at idx to val.
+   *
+   * Throws IndexError if not found.
+   **/
+
+  setAt(idx: number, val: string): void {
+  };
+
+  /** insertAt(idx, val): add node w/val before idx.
+   *
+   * Throws IndexError if not found.
+   **/
+
+  insertAt(idx: number, val: string): void {
+  };
+
+  /** removeAt(idx): return & remove item at idx,
+   *
+   * Throws IndexError if not found.
+   **/
+
+  removeAt(idx: number): string {
+    return "x";
+  }
+
+  /** toArray (useful for tests!) */
+
+  toArray(): string[] {
+    const out = [];
+    let current = this.head;
+
+    while (current) {
+      out.push(current.val);
+      current = current.next;
+    }
+
+    return out;
+  }
+}
+
+
+export {
+  IndexError,
+  LLStr,
+  NodeStr,
+};
